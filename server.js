27e49c438cfef1a079fa7547fd83699795558a2a
Cxlopez <cristian4402@hotmail.com>
2023-02-11 22:44:49 -0500

    add post to api/fruits

--- server.js
+++ server.js
@@ -5,13 +5,25 @@ const morgan = require('morgan');
 //SETUP AND MIDDLEWARES
 const app = express();
 const port = 8000;
-
-app.use(morgan('dev'));
+app.use(morgan('dev')); //Requests logger
+app.use(express.json()); //Allows requests that include json body
 
 //ROUTES / ENDPOINTS
 app.get ('/', (req, res) => {
   res.send("Hello world!");
 })
+
+//CRUD Operations
+//CREATE - POST
+app.post('/api/fruits', (req, res) => {
+  console.log(req.body);
+  res.status(201).send({message: 'Created!', fruit: req.body})
+})
+//READ - GET
+
+//UPDATE - PUT
+
+//DELETE - DELETE
 
 //LISTENER 
 app.listen(port, () => {
