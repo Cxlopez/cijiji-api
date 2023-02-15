915d26099058c7be36033673f414d7298146f698
Cxlopez <cristian4402@hotmail.com>
2023-02-14 20:15:22 -0500

    add crud functions

--- server.js
+++ server.js
@@ -2,6 +2,20 @@
 const express = require("express");
 const morgan = require('morgan');
 
+const fruitsDB = {
+  '1a': {
+    name: 'mango',
+    color: 'yellow',
+    image: "🥭"
+  },
+
+  '2b': {
+    name: 'blueberry',
+    color: 'blue',
+    image: "🫐"
+  }
+}
+
 //SETUP AND MIDDLEWARES
 const app = express();
 const port = 8000;
@@ -13,17 +27,74 @@ app.get ('/', (req, res) => {
   res.send("Hello world!");
 })
 
+app.get('/home', (req, res) => {
+  res.status(200).send({ message: 'welcome home'});
+})
+
 //CRUD Operations
 //CREATE - POST
 app.post('/api/fruits', (req, res) => {
-  console.log(req.body);
+  const {name, color, image} = req.body;
+  if(!name || !color || !image) {
+    return res.status(400).send({ message: 'provide name, color, image to create a fruit' });
+  }
+  
+  const id = Math.floor(Math.random() * 10);
+
+  fruitsDB[id] = {
+    name,
+    color,
+    image
+  }
+
   res.status(201).send({message: 'Created!', fruit: req.body})
 })
+
+
 //READ - GET
+//READ ALL
+app.get('/api/fruits', (req, res) => {
+  res.status(200).send({ message: 'List of all fruits', fruits: fruitsDB})
+})
+
+//READ ONE
+app.get('/api/fruits/:id', (req, res) => {
+  const { id } = req.params
+  const fruit = fruitsDB[id];
+  if (!fruit) {
+    return res.status(404).send({ message: 'Sorry fruit not found' });
+  }
+
+  res.status(200).send({ message: 'Here is you fruit', fruits: fruitsDB[id]});
+})
+
 
 //UPDATE - PUT
+app.put('/api/fruits/:id', (req, res) => {
+  const {name, color, image} = req.body;
+  if(!name || !color || !image) {
+    return res.status(400).send({ message: 'provide name, color, image to create a fruit' });
+  }
+  
+  const { id } = req.params;
+
+  fruitsDB[id] = {
+    name,
+    color,
+    image
+  }
+
+  res.status(201).send({message: 'Updated!', fruit: req.body})
+})
+
 
 //DELETE - DELETE
+app.delete('/api/fruits/:id', (req, res) => {
+  const { id } = req.params;
+  delete fruitsDB[id];
+  res.status(204).send();
+});
+
 
 //LISTENER 
 app.listen(port, () => {
