4d55c79409b9b644f8c09b578b85c69374fe192f
Cxlopez <cristian4402@hotmail.com>
2023-03-10 22:21:40 -0500

    add more adsSeeds

--- db/seeds/usersSeeds.js
+++ db/seeds/usersSeeds.js
@@ -1,4 +1,6 @@
 module.exports = `
-  INSERT INTO users (email, password, name) VALUES ('user1@email.com', 'abc123', 'steve');
-  INSERT INTO users (email, password, name) VALUES ('user2@email.com', 'abc123', 'edwin');
+  INSERT INTO users (email, password, name) VALUES ('user1@email.com', 'abc123', 'Manna');
+  INSERT INTO users (email, password, name) VALUES ('user2@email.com', 'abc123', 'Edwin');
+  INSERT INTO users (email, password, name) VALUES ('user3@email.com', 'abc123', 'John');
+  INSERT INTO users (email, password, name) VALUES ('user4@email.com', 'abc123', 'Peter');
 `;
