21bb003e786b4a9f756b29f88e0e595c182a78a8
Cxlopez <cristian4402@hotmail.com>
2023-03-13 19:06:41 -0400

    add adSeeds data

--- db/seeds/adsSeeds.js
+++ db/seeds/adsSeeds.js
@@ -1,13 +1,9 @@
 module.exports = `
   INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (1, 'ferrari', 'https://cdn.ferrari.com/cms/network/media/img/resize/63e4c83a7c2ea600233d4a4d-ferrari-812-superfast-approved-2023-mobile?width=375&height=350', 'Red ferrari for sale! 10/10 condition, it will annoy your neighbours!', 'autos', 500);
 
-  INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (1, 'Lamborghini', 'https://img.sm360.ca/images/web/dilawri-group-of-companies/3772/home-top-highlights-lamborghini-lineup1655120875536.jpg', 'Green Lamborghini! It doesn't come with an engine but it looks cool!', 'autos', 1500);
-
   INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (1, 'Delorean', 'https://clickamericana.com/wp-content/uploads/Front-of-DeLorean-DMC-12-car-Credit-moisseyev-Deposit-Photos.jpg', 'The Delorean for sale 8/10 condition! never be late for work with this speedster', 'autos', 1985);
 
   INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (2, 'Gaming pc!', 'https://i.etsystatic.com/29572523/r/il/9f3f1a/4369569120/il_570xN.4369569120_7g7x.jpg', 'Custom Gaming pc 10/10, tired iof paying heating bills? just throw this Computer in your room, will feel like a hot summer day!', 'tech', 5000);
-
-  INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (2, 'Gaming mouse!', 'https://multimedia.bbycastatic.ca/multimedia/products/1500x1500/135/13506/13506088.jpg', 'Gaming mouse for sale! it won't make you better but it has cool lights!', 'tech', 30);
 
   INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (2, 'VR headset', 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4H0Kb?ver=e6a4&q=90&m=6&h=270&w=270&b=%23FFFFFFFF&f=jpg&o=f&aim=true', 'Do you enjoy feeling dizzy?? well look no further! My vr headset will do just that!', 'tech', 150);
 
@@ -19,13 +15,6 @@ module.exports = `
 
   INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (3, 'Friendly house cat!', 'https://i.natgeofe.com/n/2fcdf053-13ad-40c9-a438-7e55856002ec/jaguar_thumb_2x3.JPG', 'Great with kids!!', 'pets', 2);
 
-  INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (3, 'Pet Rat', 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-f3qxzs_4923c203.jpeg', 'He's a great cook! and good with other pets!', 'pets', 9999);
-
-  INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (3, 'Pet Turtle', 'https://cdn11.bigcommerce.com/s-5vgm33yds7/images/stencil/1280x1280/products/2301/745/1735d59c7d0a244f44d127067cb8152a07faa873__23510.1597339658.jpg?c=1', 'It's pretty fast for a turtle!', 'pets', 700);
-
-  INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (4, 'Selling house!', 'https://cdn.houseplansservices.com/product/dt0biqq4ga38s7rdm8tjnbglkp/w800x533.jpg?v=2', 'It's a house, you can live in it!', 'homes', 250,000);
-
-  INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (4, 'Toronto home for sale!', 'https://media.istockphoto.com/id/488613191/photo/bed-made-of-cartons-of-a-homeless-man.jpg?s=612x612&w=0&k=20&c=FFioOmWC16MTl0qCiENpB6NOvwIqxqC4Wv6aOjN1cWg=', 'Has great natural lighting! In the heart of downtown!', 'homes', 2,500,000);
 
   INSERT INTO ads (user_id, title, thumbnail_url, description, category, price) VALUES (4, 'Pearphone 19', 'https://static.wixstatic.com/media/97b8bb_4bfe809397724b64a048003b07520ac1~mv2.png/v1/fill/w_315,h_415,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Screen%20Shot%202020-01-29%20at%209_37_30%20PM.png', 'The new Pearphone 19, 10/10 condition! very low on calories!', 'tech', 850);
 
